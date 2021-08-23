23ee0a4840244b41f47ce2b22043dbcaf16d6987
kirillsamuilov <3025889+kirillsamuilov@users.noreply.github.com>
2021-08-23 15:36:01 +0200

    header improvement

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,19 +1,22 @@
 <header class="c-header c-header-light px-3">
+    <button class="c-header-toggler c-class-toggler d-lg-none mfe-auto" type="button" data-target="#sidebar"
+            data-class="c-sidebar-show">
+        <i class="cil-hamburger-menu"></i>
+    </button>
     <ul class="c-header-nav ml-auto">
-        <li class="c-header-nav-item dropdown">
-            <a class="c-header-nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-               data-toggle="dropdown" aria-expanded="false">
-                <i class="cil-user"></i>
+        <li class="c-header-nav-item px-3">
+            <i class="c-icon cil-user mr-2"></i>
+            {{ auth()->user()->name }}
+        </li>
+        <li class="c-header-nav-item px-3">
+            <a class="c-header-nav-link" href="{{ route('logout') }}"
+               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                <i class="c-icon cil-account-logout mr-2"></i>
+                {{ __('Logout') }}
             </a>
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="{{ route('logout') }}"
-                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
-                </a>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                    @csrf
-                </form>
-            </div>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
         </li>
 
     </ul>
