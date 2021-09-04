cd210d9c8ffe094b902c12c754e85b94b3e3c068
kirillsamuilov <3025889+kirillsamuilov@users.noreply.github.com>
2021-09-04 17:09:41 +0200

    Project Api

--- app/Http/Controllers/Api/V1/ProjectController.php
+++ app/Http/Controllers/Api/V1/ProjectController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateUpdateProjectRequest;
 use App\Http\Resources\V1\ProjectResource;
 use App\Models\Project;
-use App\Models\Task;
-use Illuminate\Http\Request;
+use App\Services\SpatieMediaLibrary\AddMediaToModel;
 
 class ProjectController extends Controller
 {
@@ -30,12 +30,20 @@ class ProjectController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  CreateUpdateProjectRequest  $request
+     * @param  AddMediaToModel  $addMediaToModel
      * @return \Illuminate\Http\Response
+     * @throws \Illuminate\Auth\Access\AuthorizationException
      */
-    public function store(Request $request)
+    public function store(CreateUpdateProjectRequest $request, AddMediaToModel $addMediaToModel)
     {
-        //
+        $this->authorize('create', Project::class);
+
+        $project = Project::create($request->except('media'));
+
+        $addMediaToModel($request->input('media', []), $project);
+
+        return new ProjectResource($project);
     }
 
     /**
@@ -52,23 +60,36 @@ class ProjectController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  CreateUpdateProjectRequest  $request
+     * @param  Project  $project
+     * @param  AddMediaToModel  $addMediaToModel
      * @return \Illuminate\Http\Response
+     * @throws \Illuminate\Auth\Access\AuthorizationException
      */
-    public function update(Request $request, $id)
+    public function update(CreateUpdateProjectRequest $request, Project $project, AddMediaToModel $addMediaToModel)
     {
-        //
+        $this->authorize('update', $project);
+
+        $project->update($request->except('media'));
+
+        $addMediaToModel($request->input('media', []), $project);
+
+        return new ProjectResource($project);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  Project $project
      * @return \Illuminate\Http\Response
+     * @throws \Illuminate\Auth\Access\AuthorizationException
      */
-    public function destroy($id)
+    public function destroy(Project $project)
     {
-        //
+        $this->authorize('delete', $project);
+
+        $project->delete();
+
+        return response()->json(['message' => 'Project deleted']);
     }
 }
