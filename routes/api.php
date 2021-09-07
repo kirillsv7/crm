d380881dd6cbe3bee38f4637fb342e14b1fdacdd
kirillsamuilov <3025889+kirillsamuilov@users.noreply.github.com>
2021-09-07 23:59:12 +0200

    Laravel Sanctum integration

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['prefix' => 'v1'], function () {
+Route::group([
+    'prefix'     => 'v1',
+    'middleware' => 'auth:sanctum',
+], function () {
     Route::apiResources([
         'user'    => \App\Http\Controllers\Api\V1\UserController::class,
         'client'  => \App\Http\Controllers\Api\V1\ClientController::class,
