c8d2ef1cddd8dec415c01f3de0de33efc0e82e26
kirillsamuilov <3025889+kirillsamuilov@users.noreply.github.com>
2021-10-01 01:54:44 +0200

    CoreUI moved to package.json

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,4 @@
 require('./bootstrap');
+import coreui from "@coreui/coreui"
 import Dropzone from "dropzone";
 Dropzone.autoDiscover = false;
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -3,22 +3,15 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>{{ isset($title) ? $title . ' :: ' . config('app.name', 'Laravel') : config('app.name', 'Laravel') }}</title>
-
-    <!-- CoreUI CSS -->
-    <link rel="stylesheet" href="https://unpkg.com/@coreui/coreui@3.4.0/dist/css/coreui.min.css"
-          crossorigin="anonymous">
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body class="c-app">
     <div class="c-wrapper c-fixed-components">
         @yield('content')
     </div>
+
+    <script src="{{ mix('js/app.js') }}" defer></script>
+    @stack('scripts')
 </body>
 </html>
