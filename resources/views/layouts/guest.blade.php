ad52f93123f403a056796fe9672dea6923bc6761
kirillsamuilov <3025889+kirillsamuilov@users.noreply.github.com>
2021-10-28 01:47:27 +0200

    Guest layout

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ isset($title) ? $title . ' :: ' . config('app.name', 'Laravel') : config('app.name', 'Laravel') }}</title>
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="c-app">
+    <div class="c-wrapper c-fixed-components">
+        @yield('content')
+    </div>
+    <script src="{{ mix('js/app.js') }}" defer></script>
+    @stack('scripts')
+</body>
+</html>
