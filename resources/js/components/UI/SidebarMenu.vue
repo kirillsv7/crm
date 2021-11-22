661cff6e5ab89c7a499e0d9c49c79d2e0604fdd4
kirillsamuilov <3025889+kirillsamuilov@users.noreply.github.com>
2021-11-22 23:31:00 +0100

    SidebarMenu

--- resources/js/components/UI/SidebarMenu.vue
+++ resources/js/components/UI/SidebarMenu.vue
@@ -14,138 +14,105 @@
           Dashboard
         </router-link>
       </li>
-      <li class="c-sidebar-nav-item c-sidebar-nav-dropdown">
-        <a class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
-          <i class="c-sidebar-nav-icon cil-user"></i>
+      <li class="c-sidebar-nav-item">
+        <router-link
+            class="c-sidebar-nav-link"
+            :class="{'c-active': this.$route.name == 'user.index'}"
+            :to="{name:'user.index'}">
+          <i class="c-sidebar-nav-icon cil-list-rich"></i>
           Users
-        </a>
-        <ul class="c-sidebar-nav-dropdown-items">
-          <li class="c-sidebar-nav-item">
-            <router-link
-                class="c-sidebar-nav-link"
-                :class="{'c-active': this.$route.name == 'user.index'}"
-                :to="{name:'user.index'}">
-              <i class="c-sidebar-nav-icon cil-list-rich"></i>
-              User list
-            </router-link>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <router-link
-                class="c-sidebar-nav-link"
-                :class="{'c-active': this.$route.name == 'user.create'}"
-                :to="{name:'user.create'}">
-              <i class="c-sidebar-nav-icon cil-user-plus"></i>
-              Add user
-            </router-link>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <router-link
-                class="c-sidebar-nav-link"
-                :class="{'c-active': this.$route.name == 'user.deleted'}"
-                :to="{name:'user.deleted'}">
-              <i class="c-sidebar-nav-icon cil-user-x"></i>
-              Deleted users
-            </router-link>
-          </li>
-        </ul>
+        </router-link>
       </li>
-      <li class="c-sidebar-nav-item c-sidebar-nav-dropdown">
-        <a class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
-          <i class="c-sidebar-nav-icon cil-address-book"></i>
+      <li class="c-sidebar-nav-item">
+        <router-link
+            class="c-sidebar-nav-link"
+            :class="{'c-active': this.$route.name == 'user.create'}"
+            :to="{name:'user.create'}">
+          <i class="c-sidebar-nav-icon cil-user-plus"></i>
+          Add user
+        </router-link>
+      </li>
+      <li class="c-sidebar-nav-item">
+        <router-link
+            class="c-sidebar-nav-link"
+            :class="{'c-active': this.$route.name == 'user.deleted'}"
+            :to="{name:'user.deleted'}">
+          <i class="c-sidebar-nav-icon cil-user-x"></i>
+          Deleted users
+        </router-link>
+      </li>
+      <li class="c-sidebar-nav-item">
+        <router-link
+            class="c-sidebar-nav-link"
+            :class="{'c-active': this.$route.name == 'client.index'}"
+            :to="{name:'client.index'}">
+          <i class="c-sidebar-nav-icon cil-list-rich"></i>
           Clients
-        </a>
-        <ul class="c-sidebar-nav-dropdown-items">
-          <li class="c-sidebar-nav-item">
-            <router-link
-                class="c-sidebar-nav-link"
-                :class="{'c-active': this.$route.name == 'client.index'}"
-                :to="{name:'client.index'}">
-              <i class="c-sidebar-nav-icon cil-list-rich"></i>
-              Client list
-            </router-link>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <router-link
-                class="c-sidebar-nav-link"
-                :class="{'c-active': this.$route.name == 'client.create'}"
-                :to="{name:'client.create'}">
-              <i class="c-sidebar-nav-icon cil-plus"></i>
-              Add client
-            </router-link>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <router-link
-                class="c-sidebar-nav-link"
-                :class="{'c-active': this.$route.name == 'client.deleted'}"
-                :to="{name:'client.deleted'}">
-              <i class="c-sidebar-nav-icon cil-user-x"></i>
-              Deleted clients
-            </router-link>
-          </li>
-        </ul>
+        </router-link>
       </li>
-      <li class="c-sidebar-nav-item c-sidebar-nav-dropdown">
-        <a class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
-          <i class="c-sidebar-nav-icon cil-briefcase"></i>
+      <li class="c-sidebar-nav-item">
+        <router-link
+            class="c-sidebar-nav-link"
+            :class="{'c-active': this.$route.name == 'client.create'}"
+            :to="{name:'client.create'}">
+          <i class="c-sidebar-nav-icon cil-plus"></i>
+          Add client
+        </router-link>
+      </li>
+      <li class="c-sidebar-nav-item">
+        <router-link
+            class="c-sidebar-nav-link"
+            :class="{'c-active': this.$route.name == 'client.deleted'}"
+            :to="{name:'client.deleted'}">
+          <i class="c-sidebar-nav-icon cil-user-x"></i>
+          Deleted clients
+        </router-link>
+      </li>
+      <li class="c-sidebar-nav-item">
+        <router-link
+            class="c-sidebar-nav-link"
+            :class="{'c-active': this.$route.name == 'project.index'}"
+            :to="{name:'project.index'}">
+          <i class="c-sidebar-nav-icon cil-list-rich"></i>
           Projects
-        </a>
-        <ul class="c-sidebar-nav-dropdown-items">
-          <li class="c-sidebar-nav-item">
-            <router-link
-                class="c-sidebar-nav-link"
-                :class="{'c-active': this.$route.name == 'project.index'}"
-                :to="{name:'project.index'}">
-              <i class="c-sidebar-nav-icon cil-list-rich"></i>
-              Project list
-            </router-link>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <router-link
-                class="c-sidebar-nav-link"
-                :class="{'c-active': this.$route.name == 'project.create'}"
-                :to="{name:'project.create'}">
-              <i class="c-sidebar-nav-icon cil-plus"></i>
-              Add project
-            </router-link>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <router-link
-                class="c-sidebar-nav-link"
-                :class="{'c-active': this.$route.name == 'project.deleted'}"
-                :to="{name:'project.deleted'}">
-              <i class="c-sidebar-nav-icon cil-minus"></i>
-              Deleted projects
-            </router-link>
-          </li>
-        </ul>
+        </router-link>
       </li>
-      <li class="c-sidebar-nav-item c-sidebar-nav-dropdown">
-        <a class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
-          <i class="c-sidebar-nav-icon cil-task"></i>
+      <li class="c-sidebar-nav-item">
+        <router-link
+            class="c-sidebar-nav-link"
+            :class="{'c-active': this.$route.name == 'project.create'}"
+            :to="{name:'project.create'}">
+          <i class="c-sidebar-nav-icon cil-plus"></i>
+          Add project
+        </router-link>
+      </li>
+      <li class="c-sidebar-nav-item">
+        <router-link
+            class="c-sidebar-nav-link"
+            :class="{'c-active': this.$route.name == 'project.deleted'}"
+            :to="{name:'project.deleted'}">
+          <i class="c-sidebar-nav-icon cil-minus"></i>
+          Deleted projects
+        </router-link>
+      </li>
+      <li class="c-sidebar-nav-item">
+        <a class="c-sidebar-nav-link" href="/task">
+          <i class="c-sidebar-nav-icon cil-list-rich"></i>
           Tasks
         </a>
-        <ul class="c-sidebar-nav-dropdown-items">
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="/task">
-              <i class="c-sidebar-nav-icon cil-list-rich"></i>
-              Task list
-            </a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="/task/create">
-              <i class="c-sidebar-nav-icon cil-plus"></i>
-              Add task
-            </a>
-          </li>
-          <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="/task/deleted">
-              <i class="c-sidebar-nav-icon cil-minus"></i>
-              Deleted tasks
-            </a>
-          </li>
-        </ul>
       </li>
-
+      <li class="c-sidebar-nav-item">
+        <a class="c-sidebar-nav-link" href="/task/create">
+          <i class="c-sidebar-nav-icon cil-plus"></i>
+          Add task
+        </a>
+      </li>
+      <li class="c-sidebar-nav-item">
+        <a class="c-sidebar-nav-link" href="/task/deleted">
+          <i class="c-sidebar-nav-icon cil-minus"></i>
+          Deleted tasks
+        </a>
+      </li>
       <i class="cis-playlist-add-check"></i>
     </ul>
     <button class="c-sidebar-minimizer c-class-toggler" type="button" data-target="_parent"
