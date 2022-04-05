01662affba364801914450b56abbff0a52440d4c
kirillsamuilov <3025889+kirillsamuilov@users.noreply.github.com>
2022-04-06 00:13:49 +0200

    Pagination simplified

--- resources/js/components/UI/PaginationElement.vue
+++ resources/js/components/UI/PaginationElement.vue
@@ -9,7 +9,7 @@
           <router-link
               class="page-link"
               v-if="link.url && !link.active"
-              :to="transformApiUrl(link.url)"
+              :to="link.url"
               v-html="link.label"
               :aria-current="link.active ? 'page' : ''"
           />
@@ -22,47 +22,12 @@
 </template>
 
 <script>
-import {useRoute, useRouter} from "vue-router";
-import {onUpdated} from "vue";
-
 export default {
   props: {
     pagination: {
       required: true,
       type: Object,
     },
-  },
-
-  setup(props) {
-    const route = useRoute()
-    const router = useRouter()
-
-    onUpdated(() => {
-      if (props.pagination.current_page > props.pagination.last_page) {
-        const newUrl = transformApiUrl(props.pagination.links[0].url)
-        newUrl.query.page = props.pagination.last_page
-        router.push(newUrl)
-      }
-    })
-
-    const transformApiUrl = (link) => {
-      const url = new URL(link)
-      const params = new URLSearchParams(url.search)
-      const newParams = {}
-
-      params.forEach(function (value, key) {
-        newParams[key] = value
-      })
-
-      return {
-        path: route.path,
-        query: newParams
-      }
-    }
-
-    return {
-      transformApiUrl
-    }
   }
 }
 </script>
