80086e7c420b428e17c4da104ff82a8b7b1e21c6
kirillsamuilov <3025889+kirillsamuilov@users.noreply.github.com>
2022-05-03 02:50:16 +0200

    FileUpload process improvement

--- resources/js/components/UI/Form/FileUpload.vue
+++ resources/js/components/UI/Form/FileUpload.vue
@@ -1,17 +1,18 @@
 <template>
   <FilePond
-      label-idle="Drop files here..."
+      :label-idle="labelIdle"
       ref="filePond"
       allow-multiple="true"
       accepted-file-types="image/jpeg, image/png"
       instant-upload="false"
+      max-files="10"
       imagePreviewHeight="100"
       @updatefiles="handleUpdateFiles"
   />
 </template>
 
 <script>
-import {ref, watch} from "vue";
+import {onMounted, ref, watch} from "vue";
 import vueFilePond from "vue-filepond"
 import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type"
 import FilePondPluginImagePreview from "filepond-plugin-image-preview"
@@ -38,10 +39,15 @@ export default {
 
   setup(props) {
     const filePond = ref(null)
+    const labelIdle = ref('')
 
     const handleUpdateFiles = (files) => {
-      if (files.length)
+      if (files.length) {
         props.model.files = files.map(files => files.file)
+        labelIdle.value = `Drop files here - ${files.length}/${filePond.value.maxFiles} files added`
+      } else {
+        labelIdle.value = `Drop files here - max ${filePond.value.maxFiles} files`
+      }
     }
 
     const removeUploadedFiles = () => {
@@ -49,10 +55,13 @@ export default {
       delete props.model.files
     }
 
+    onMounted(() => labelIdle.value = `Drop files here - max ${filePond.value.maxFiles} files`)
+
     watch(() => props.saved, () => removeUploadedFiles())
 
     return {
       filePond,
+      labelIdle,
       handleUpdateFiles,
     }
   }
