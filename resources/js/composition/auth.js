abe89e09cc3425fa17b512996ed3d84e36ec4406
kirillsamuilov <3025889+kirillsamuilov@users.noreply.github.com>
2022-05-08 15:18:05 +0200

    Auth refactoring

--- resources/js/composition/auth.js
+++ resources/js/composition/auth.js
@@ -9,24 +9,19 @@ export default function useAuth() {
     const user = ref({})
     const errors = ref({})
 
-    const postLogin = () => {
-        axios.get('/sanctum/csrf-cookie')
-            .then(() => {
-                axios.post('/login', login.value)
-                    .then(() => {
-                        router.push({name: 'dashboard'})
-                    })
-                    .catch((e) => {
-                        errors.value = e.response.data.errors
-                    })
-            })
+    const postLogin = async () => {
+        await axios.get('/sanctum/csrf-cookie')
+        try {
+            await axios.post('/login', login.value)
+            await router.push({name: 'dashboard'})
+        } catch (e) {
+            errors.value = e.response.data.errors
+        }
     }
 
-    const postLogout = () => {
-        axios.post('/logout')
-            .then(() => {
-                router.push({name: 'auth.login'})
-            })
+    const postLogout = async () => {
+        await axios.post('/logout')
+        await router.push({name: 'auth.login'})
     }
 
     return {
